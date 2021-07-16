c76cd9da23a48ca904667b4c54a9903e3f463046
tianxia <josesky@live.cn>
2021-07-16 15:57:01 +0800

    add download.sh

---
+++ download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+
+release=$(lsb_release -d | cut -d : -f 2)
+echo -e "Your system version $release"
+echo -e "${green}===> downling... "
+cd $HOME
+
+app='https://codeload.github.com/josesky/work_space/zip/refs/heads/main'
+curl  -O ${app} work_space.tar.gz 
+
+
+
